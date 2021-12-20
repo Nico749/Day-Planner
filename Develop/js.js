53f11e0ef80a1f3474df48d8d2022c02b34bf553
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-20 23:24:17 +1100

    major changes js

--- Develop/js.js
+++ Develop/js.js
@@ -3,92 +3,46 @@ var day =document.getElementById("currentDay")
 var actualDay=moment().format("Do MMMM YYYY")
 day.textContent=actualDay   
 
+var textArea=document.querySelector("#task1")
+    textArea.value=localStorage.getItem("Description")
+    
+var textArea1=document.querySelector("#shopping-input1")
+    textArea1.value=localStorage.getItem("Description1")
 
-//generate input to insert a new event
-$(document).ready(function () {
-    $(".add_exp1").click(function(){
-        $(".exp").append('<br><br><br>'+
-        '<input type="input" style="width: 60%;" class="form-control" id="hour" placeholder="Hour (00:00)">'+
-        '<br>'+
-        '<input type="input" style="width: 60%;" class="form-control" id="description" placeholder="Description">'+
-        '<br><br><br>'+ `<button type="done" id ="donebutton" class="btn btn-primary">Done</button>`);      
 
-//save data in local storage
-        var btn=document.getElementById("donebutton")
-        
-        btn.addEventListener("click", function(event) {
-        event.preventDefault();
-        var description = document.querySelector("#description").value;
-        var time=document.querySelector("#hour").value;
-        localStorage.setItem("Event-description", description);
-        localStorage.setItem("Time",time)
-//clean input fields        
-        document.getElementById("hour").value=""
-        document.getElementById("description").value=""
-
-        renderScore()
-
+var myBtn=document.getElementById("save")
+myBtn.addEventListener("click",function(event){
+event.preventDefault()
+var description=document.querySelector("#task1").value
+localStorage.setItem("Description",description)
 })
 
+var myBtn1=document.getElementById("save1")
+myBtn1.addEventListener("click",function(event){
+    event.preventDefault()
+     var description1=document.querySelector("#shopping-input1").value
+    localStorage.setItem("Description1",description1)
 })
 
-}) 
 
 
-var eight=document.getElementById("ciao1").innerHTML
-var value =16
+
+
+
+//var eight=document.getElementById("ciao1").innerHTML
+var value =20
 var time=moment().format("H")
 if (time>value){
 
 $("#firstrow,#secondrow").addClass("future")
-$("#thirdrow,#fourthrow").addClass("past")}
+$("#hour").css("background-color","yellow")
+$("#hour1").css("background-color","yellow")
+//$("#8").addClass("saveBtn")
+//$("#8,#9,#10,#11,#12,#13,#14,#15,#16,#17,#18,#19,#20,#21,#22,#23").addClass("saveBtn ")
+
+}
 
 
-console.log(eight)
 
-
-
-
-
-function renderScore() {
-    var description = localStorage.getItem("Event-description");
-    var timeevent=JSON.parse(localStorage.getItem("Time"))
-    console.log(timeevent)
-    
-    if (timeevent=="8"){
-        document.getElementById("8").innerHTML = description;}
-    else if(timeevent=="9") { 
-        document.getElementById("9").innerHTML = description;}
-    else if(timeevent=="10") { 
-        document.getElementById("10").innerHTML = description;}
-    else if(timeevent=="11") { 
-        document.getElementById("11").innerHTML = description;}
-    else if(timeevent=="12") { 
-        document.getElementById("12").innerHTML = description;}
-    else if(timeevent=="13") { 
-        document.getElementById("13").innerHTML = description;}
-    else if(timeevent=="14") { 
-        document.getElementById("14").innerHTML = description;}
-    else if(timeevent=="15") { 
-        document.getElementById("15").innerHTML = description;}
-    else if(timeevent=="16") { 
-        document.getElementById("16").innerHTML = description;}
-    else if(timeevent=="17") { 
-        document.getElementById("17").innerHTML = description;}
-    else if(timeevent=="18") { 
-        document.getElementById("18").innerHTML = description;}
-    else if(timeevent=="19") { 
-        document.getElementById("19").innerHTML = description;}
-    else if(timeevent=="20") { 
-        document.getElementById("20").innerHTML = description;}
-    else if(timeevent=="21") { 
-        document.getElementById("21").innerHTML = description;}
-    else if(timeevent=="22") { 
-        document.getElementById("22").innerHTML = description;}
-    else if(timeevent=="23") { 
-        document.getElementById("23").innerHTML = description;}                                
-
-    
-   } 
 
 
